bbc215c0923268f3f60fe22a350beb11fb39af9b
Aleksandr Pachaev <polarisf@yandex.ru>
2026-09-19 16:51:00 +0300

    Fixed monogame trimming.

--- TankRacerViewer.DesktopGL/Program.cs
+++ TankRacerViewer.DesktopGL/Program.cs
@@ -1,8 +1,29 @@
-﻿using DesktopCommon;
+﻿using System;
+#if !DEBUG
+using System.IO;
+#endif
+
+using DesktopCommon;
+
+using Microsoft.Xna.Framework.Content;
 
 using TankRacerViewer.Core;
 using TankRacerViewer.DesktopGL;
 
-using var game = new MainWindow(new DesktopStorage("TankRacerViewer"),
+try
+{
+    // Workaround for Monogame trimming.
+    var listReaderFullName = typeof(ListReader<char>).FullName.Replace("9.0", "8.0");
+    ContentTypeReaderManager.AddTypeCreator(listReaderFullName, () => new ListReader<char>());
+
+    using var game = new MainWindow(new DesktopStorage("TankRacerViewer"),
     new DesktopUrlOpener(), new NativeFileDialog());
-game.Run();
+    game.Run();
+}
+catch (Exception exception)
+{
+#if !DEBUG
+    File.WriteAllText("crash.log", exception.ToString());
+#endif
+    throw;
+}
